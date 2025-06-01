88685e5e0e6bffad75329470427d11b0924fda07
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-01 14:23:29 +0300

    Add `Error` impl.

---
+++ long-arithmetic/source/error.cpp
@@ -0,0 +1,16 @@
+#include "error.hpp"
+
+Error make_error( ErrorCode code, const std::string& message ) {
+    return Error{ code, message };
+}
+
+const ErrorCode& get_error_code( const Error& error ) { return error.code; }
+
+const std::string& get_error_message( const Error& error ) {
+    return error.message;
+}
+
+bool is_ok( const Error& error ) {
+    ErrorCode code = get_error_code( error );
+    return code == ErrorCode::OK;
+}
