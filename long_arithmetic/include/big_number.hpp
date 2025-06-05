9da72eb120e4ff7130fd752f0e4aaa9dffecd157
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-05 14:12:57 +0300

    Add `abs` to `big_number.hpp`

--- long_arithmetic/include/big_number.hpp
+++ long_arithmetic/include/big_number.hpp
@@ -32,6 +32,8 @@ bool is_lower_than( const BigNumber& left, const BigNumber& right );
 
 bool is_greater_than( const BigNumber& left, const BigNumber& right );
 
+BigNumber abs( const BigNumber& operand );
+
 BigNumber neg( const BigNumber& operand );
 
 BigNumber add( const BigNumber& augend, const BigNumber& addend );
