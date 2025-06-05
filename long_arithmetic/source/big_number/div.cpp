694861aefad5c5c97531adb19e0c94b365a56aef
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-05 15:15:49 +0300

    Refactor `div.cpp`

--- long_arithmetic/source/big_number/div.cpp
+++ long_arithmetic/source/big_number/div.cpp
@@ -3,15 +3,15 @@
 
 #include "big_number.hpp"
 #include "constructors.hpp"
-#include "error.hpp"
 #include "getters.hpp"
 
-const int32_t DIV_PRECISION = 100; // should be enough for most cases, but the
-                                   // real precision calculated differently
-const BigNumber ZERO = make_big_number( "0" );
-const BigNumber ONE = make_big_number( "1" );
+const int32_t DIV_PRECISION = 100; // should be enough, doesnt real
 const Error DIVISION_BY_ZERO =
     make_error( CALCULATION_ERROR, "Division by zero" );
+
+int32_t compute_max_exp( const BigNumber& number ) {
+    return get_exponent( number ) + get_size( number ) - 1;
+}
 
 BigNumber div( const BigNumber& x, const BigNumber& y ) {
     if ( is_equal( x, ZERO ) ) { return ZERO; }
@@ -19,22 +19,20 @@ BigNumber div( const BigNumber& x, const BigNumber& y ) {
     if ( is_equal( y, ONE ) ) { return x; }
     if ( is_equal( x, y ) ) { return ONE; }
 
-    int32_t a_max_exp = get_exponent( x ) + get_size( x ) - 1;
-    int32_t b_max_exp = get_exponent( y ) + get_size( y ) - 1;
-
-    BigNumber b = from_scratch( get_chunks( y ),
-                                get_exponent( y ) + ( a_max_exp - b_max_exp ),
-                                false,
-                                DEFAULT_ERROR );
-
-    int32_t current_exp = a_max_exp;
-    mul_chunk b0 = get_chunks( b ).back();
+    BigNumber b = from_scratch(
+        get_chunks( y ),
+        get_exponent( y ) + ( compute_max_exp( x ) - compute_max_exp( y ) ),
+        false,
+        DEFAULT_ERROR );
 
     BigNumber a = abs( x );
     mul_chunk carry = 0;
     std::vector<chunk> result_chunks;
     int32_t max_chunks =
-        std::max( a_max_exp - b_max_exp + 1, 0 ) + DIV_PRECISION;
+        std::max( compute_max_exp( x ) - compute_max_exp( y ) + 1, 0 ) +
+        DIV_PRECISION;
+    int32_t current_exp = compute_max_exp( x );
+    mul_chunk b0 = get_chunks( b ).back();
 
     for ( int32_t i = 0; i <= max_chunks; ++i ) {
         if ( is_equal( a, ZERO ) ) break;
@@ -80,14 +78,11 @@ BigNumber div( const BigNumber& x, const BigNumber& y ) {
         --current_exp;
     }
 
-    int32_t result_exp = a_max_exp - b_max_exp + 1 -
-                         static_cast<int32_t>( result_chunks.size() );
+    int32_t result_exp =
+        compute_max_exp( x ) - compute_max_exp( y ) + 1 - result_chunks.size();
     std::vector<chunk> chunks_rev( result_chunks.rbegin(),
                                    result_chunks.rend() );
     std::vector<chunk> final_chunks( chunks_rev.begin(), chunks_rev.end() );
-    BigNumber result = from_scratch( final_chunks,
-                                     result_exp,
-                                     x.is_negative != y.is_negative,
-                                     make_error( OK, "" ) );
-    return result;
+    bool sign = is_negative( x ) != is_negative( y );
+    return from_scratch( final_chunks, result_exp, sign, DEFAULT_ERROR );
 }
