657635434af0943cdff6b57c32576eeeba5bba88
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-05 15:21:59 +0300

    Refactor `sqrt.cpp`

--- long_arithmetic/source/big_number/sqrt.cpp
+++ long_arithmetic/source/big_number/sqrt.cpp
@@ -1,19 +1,21 @@
-#include <stdexcept>
-
 #include "basic_arithmetic.hpp"
 #include "big_number.hpp"
 #include "constructors.hpp"
-#include "getters.hpp"
+#include "error.hpp"
 
-const BigNumber TWO = make_big_number( "2" );
+const Error ROOT_FROM_NEG =
+    make_error( CALCULATION_ERROR, "sqrt: negative number" );
+
+const int ITERATIONS = 30;
 
 BigNumber sqrt( const BigNumber& number ) {
-    if ( is_lower_than( number, ZERO ) )
-        throw std::invalid_argument( "sqrt: negative number" );
+    if ( is_lower_than( number, ZERO ) ) return make_zero( ROOT_FROM_NEG );
     if ( is_equal( number, ZERO ) || is_equal( number, ONE ) ) return number;
+
     BigNumber x = number;
     BigNumber last = ZERO;
-    for ( int i = 0; i < 30 && !( is_equal( x, last ) ); ++i ) {
+
+    for ( int i = 0; i < ITERATIONS && !( is_equal( x, last ) ); ++i ) {
         last = x;
         x = div( add( x, div( number, x ) ), TWO );
     }
