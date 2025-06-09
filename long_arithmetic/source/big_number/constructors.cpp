33dc1acac4b95ba0205e1ca6886d2095c427802f
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-10 00:14:12 +0300

    Update `from_scratch` constructor, delete obsolete `strip_zeros`

--- long_arithmetic/source/big_number/constructors.cpp
+++ long_arithmetic/source/big_number/constructors.cpp
@@ -1,50 +1,58 @@
 #include "constructors.hpp"
 
-#include "error.hpp"
+#include "big_number.hpp"
 
 namespace big_number {
-    void strip_zeros( BigNumber& num ) { // TODO: need a refactor
-        size_t pref_zeros = 0;
-        size_t suff_zeros = 0;
-        size_t sz = num.chunks.size();
-        for ( size_t i = 0; i < sz; ++i ) {
-            if ( num.chunks[i] == 0 )
-                pref_zeros++;
-            else
-                break;
+    chunk ZERO_CHUNK = 0;
+
+    int32_t compute_exponent_delta( const std::vector<chunk>& chunks ) {
+        size_t first_non_zero_chunk = chunks.size();
+        size_t last_non_zero_chunk = 0;
+
+        for ( size_t index = 0; index < chunks.size(); index++ ) {
+            chunk value = chunks[index];
+            if ( value == ZERO_CHUNK ) continue;
+
+            if ( index < first_non_zero_chunk ) first_non_zero_chunk = index;
+            if ( index > last_non_zero_chunk ) last_non_zero_chunk = index;
         }
-        for ( size_t i = sz; i > 0; --i ) {
-            if ( num.chunks[i - 1] == 0 )
-                suff_zeros++;
-            else
-                break;
-        }
-        if ( pref_zeros == sz ) {
-            num.exponent = 0;
-            num.is_negative = false;
-            num.chunks.clear();
-            return;
-        }
-        num.exponent += pref_zeros;
-        size_t new_size = sz - pref_zeros - suff_zeros;
-        if ( new_size == sz ) return;
-        num.chunks.erase( num.chunks.begin(), num.chunks.begin() + pref_zeros );
-        num.chunks.resize( new_size );
+
+        return ( first_non_zero_chunk + 1 ) -
+               ( chunks.size() - ( last_non_zero_chunk + 1 ) );
     }
 
-    BigNumber from_scratch(
-        const std::vector<chunk>& chunks,
-        int32_t exponent,
-        bool is_negative,
-        const Error& error = DEFAULT_ERROR ) { // TODO: need a refactor
+    std::vector<chunk> trim_zeros( std::vector<chunk>& chunks ) {
+        if ( chunks.empty() ) return {};
 
-        BigNumber res;
-        res.chunks = chunks;
-        res.exponent = exponent;
-        res.is_negative = is_negative;
-        res.error = error;
-        strip_zeros( res );
-        return res;
+        size_t size = chunks.size();
+        size_t first_nonzero = 0;
+        size_t last_nonzero = size;
+
+        while ( first_nonzero < size && chunks[first_nonzero] == 0 ) {
+            first_nonzero++;
+        }
+
+        if ( first_nonzero == size ) return {};
+
+        while ( last_nonzero > first_nonzero && chunks[last_nonzero - 1] == 0 )
+            last_nonzero--;
+
+        return std::vector<chunk>(
+            std::make_move_iterator( chunks.begin() + first_nonzero ),
+            std::make_move_iterator( chunks.begin() + last_nonzero ) );
+    }
+
+    BigNumber from_scratch( std::vector<chunk>& chunks,
+                            int32_t exponent,
+                            bool is_negative,
+                            const Error& error = DEFAULT_ERROR ) {
+        int32_t exponent_delta = compute_exponent_delta( chunks );
+        std::vector<chunk> trimmed_chunks = trim_zeros( chunks );
+
+        if ( trimmed_chunks.empty() ) { return make_zero(); }
+
+        return BigNumber(
+            trimmed_chunks, exponent + exponent_delta, is_negative, error );
     }
 
     BigNumber from_iterator(
