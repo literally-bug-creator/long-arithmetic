33c793a4731bb6d96c523bfb9ea990768b9b97a2
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-10 18:48:26 +0300

    Patch `output.cpp`

--- long_arithmetic/source/big_number/output.cpp
+++ long_arithmetic/source/big_number/output.cpp
@@ -1,31 +1,52 @@
+#include <string>
+
 #include "big_number.hpp"
+#include "getters.hpp"
 
 namespace big_number {
+    const std::string EMPTY_STR = "";
     const std::string MINUS_STR = "-";
+    const char ZERO_CHAR = '0';
+    const char EXPONENT_CHAR = 'e';
+
+    std::string to_string_sign( const BigNumber& number ) {
+        return is_negative( number ) ? MINUS_STR : EMPTY_STR;
+    }
+
+    std::string to_string_chunk( chunk value, bool should_filled ) {
+        std::string chunk_str = std::to_string( value );
+
+        if ( !should_filled ) { return chunk_str; }
+
+        return std::string( CHUNK_DIGITS - chunk_str.size(), ZERO_CHAR ) +
+               chunk_str;
+    }
+
+    std::string to_string_chunks( const BigNumber& number ) {
+        std::string str;
+        int32_t last_chunk_index =
+            static_cast<int32_t>( get_size( number ) ) - ONE_INT;
+
+        for ( int32_t index = last_chunk_index; index >= ZERO_INT; --index ) {
+            chunk value = get_chunk_direct( number, index );
+            str += to_string_chunk( value, index != last_chunk_index );
+        }
+
+        return str;
+    }
+
+    std::string to_string_exp( const BigNumber& number ) {
+        int32_t exp = get_exponent( number );
+
+        if ( exp == ZERO_INT ) return EMPTY_STR;
+
+        return EXPONENT_CHAR + std::to_string( exp * CHUNK_DIGITS );
+    }
 
     std::string to_string( const BigNumber& number ) {
-        if ( number.chunks.empty() ) return std::to_string( ZERO_INT );
+        if ( is_zero( number ) ) return std::to_string( ZERO_INT );
 
-        std::string result;
-        if ( number.is_negative ) result += MINUS_STR;
-
-        for ( int i = static_cast<int>( number.chunks.size() ) - ONE_INT;
-              i >= ZERO_INT;
-              --i ) {
-            std::string chunk_str = std::to_string( number.chunks[i] );
-
-            if ( i == static_cast<int>( number.chunks.size() ) - ONE_INT ) {
-                result += chunk_str;
-            } else {
-                result += std::string( CHUNK_DIGITS - chunk_str.size(), '0' ) +
-                          chunk_str;
-            }
-        }
-
-        if ( number.exponent != ZERO_INT ) {
-            result += 'e' + std::to_string( number.exponent * CHUNK_DIGITS );
-        }
-
-        return result;
+        return to_string_sign( number ) + to_string_chunks( number ) +
+               to_string_exp( number );
     }
 }
