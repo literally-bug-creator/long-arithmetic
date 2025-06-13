a42b651c41f9308e1c9f230140644fdd1f4d3773
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-13 21:41:45 +0300

    Patch `constructors` interface

--- long_arithmetic/source/big_number/constructors.hpp
+++ long_arithmetic/source/big_number/constructors.hpp
@@ -3,14 +3,10 @@
 #include "big_number.hpp"
 
 namespace big_number {
-    const BigNumber ZERO = make_big_number( {}, 0, false );
-    const BigNumber ONE = make_big_number( { 1 }, 0, false );
-    const BigNumber TWO = make_big_number( { 2 }, 0, false );
-
     BigNumber make_zero( const Error& error = get_default_error() );
 
     BigNumber internal_make_big_number( std::vector<chunk> chunks,
-                               int32_t exponent,
-                               bool is_negative,
-                               const Error& error );
+                                        int32_t exponent,
+                                        bool is_negative,
+                                        const Error& error );
 }
