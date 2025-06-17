fdfa7374f6840ad0f5e542c1a8be2b39cd136a74
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-17 23:54:26 +0300

    Add `special values` constructors, internal `BigNumber` constructor

--- long_arithmetic/source/big_number/constructors.hpp
+++ long_arithmetic/source/big_number/constructors.hpp
@@ -3,5 +3,15 @@
 #include "big_number.hpp"
 
 namespace big_number {
-    BigNumber make_zero( const Error& error );
+    BigNumber make_zero( const Error& error ) noexcept;
+
+    BigNumber make_inf( const Error& error, bool is_negative ) noexcept;
+
+    BigNumber make_nan( const Error& error ) noexcept;
+
+    BigNumber make_big_number( chunks chunks,
+                               int32_t shift,
+                               BigNumberType type,
+                               const Error& error,
+                               bool is_negative ) noexcept;
 }
