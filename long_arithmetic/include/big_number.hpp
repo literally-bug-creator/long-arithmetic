3374a3b8d6505f72eeba31a7f200563ca4e9b188
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-17 23:42:53 +0300

    Major `BigNumber` interface update

--- long_arithmetic/include/big_number.hpp
+++ long_arithmetic/include/big_number.hpp
@@ -7,26 +7,44 @@
 #include "error.hpp"
 
 namespace big_number {
-    typedef std::vector<uint8_t> digits;
     typedef uint8_t digit;
+    typedef std::vector<uint8_t> digits;
     typedef __uint128_t mul_chunk;
     typedef uint64_t chunk;
+    typedef std::vector<chunk> chunks;
 
-    const uint8_t BASE = 18;
+    const digit BASE = 18;
     const chunk MAX_CHUNK = 1000000000000000000;
-    const int16_t MAX_CHUNK_AMOUNT = 100000 / static_cast<int16_t>( BASE ) + 1;
+    const int16_t MAX_INPUT_CHUNKS = 100000 / static_cast<int16_t>( BASE ) + 1;
+    const int16_t MAX_CHUNKS = MAX_INPUT_CHUNKS + 1;
+    const int16_t MIN_CHUNKS = 1;
+    const int32_t MAX_SHIFT = 9999;
+
+    enum BigNumberType {
+        DEFAULT,
+        ZERO,
+        INF,
+        NAN,
+    };
 
     struct BigNumber {
-        std::vector<chunk> chunks;
-        Error error;
+        chunks mantissa;
         int32_t shift;
+        BigNumberType type;
+        Error error;
         bool is_negative;
     };
 
-    BigNumber make_big_number( const std::vector<uint8_t>& digits,
+    BigNumber make_big_number( const digits& digits,
                                int32_t exponent,
                                bool is_negative,
                                const Error& error ) noexcept;
+
+    BigNumber make_zero( const Error& error ) noexcept;
+
+    BigNumber make_nan( const Error& error ) noexcept;
+
+    BigNumber make_inf( const Error& error, bool is_negative ) noexcept;
 
     const Error& get_error( const BigNumber& number );
 }
