3fe7e25d88be65bb89de3b040944dfae820178b7
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-18 14:46:18 +0300

    Add `ALMOST_MAX_CHUNK`, `HALF_CHUNK`

--- long_arithmetic/source/big_number/constants.hpp
+++ long_arithmetic/source/big_number/constants.hpp
@@ -2,7 +2,10 @@
 
 #include <cstdint>
 
+#include "big_number.hpp"
+
 namespace big_number {
     const int32_t ZERO_INT = 0;
-
+    const chunk HALF_CHUNK = MAX_CHUNK / 2;
+    const chunk ALMOST_MAX_CHUNK = MAX_CHUNK - 1;
 }
