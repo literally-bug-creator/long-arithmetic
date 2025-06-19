e7635904f0336c72c99fe2fa776541925c3e41af
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-19 19:40:06 +0300

    Remove public constants

--- long_arithmetic/source/big_number/constants.hpp
+++ long_arithmetic/source/big_number/constants.hpp
@@ -2,10 +2,7 @@
 
 #include <cstdint>
 
-#include "big_number.hpp"
-
 namespace big_number {
-    const int32_t ZERO_INT = 0;
-    const chunk HALF_CHUNK = MAX_CHUNK / 2;
-    const chunk ALMOST_MAX_CHUNK = MAX_CHUNK - 1;
+    const uint8_t ZERO_INT = 0;
+    const uint8_t ONE_INT = 1;
 }
