cbe5ff01774e405e5303aedff1991bd2ba9efe39
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-19 23:20:29 +0300

    Remove constants, optimize `BigNumberType`

--- long_arithmetic/include/big_number.hpp
+++ long_arithmetic/include/big_number.hpp
@@ -1,33 +1,10 @@
 #pragma once
 
-#include <cstdint>
-#include <inttypes.h>
-#include <vector>
-
+#include "constants.hpp"
 #include "error.hpp"
 
 namespace big_number {
-    using digit = uint8_t;
-    using digits = std::vector<digit>;
-    using mul_chunk = __uint128_t;
-    using chunk = uint64_t;
-    using chunks = std::vector<chunk>;
-
-    const int32_t BASE = 18;
-    const size_t PRECISION = 100000;
-
-    const chunk MAX_CHUNK = 1000000000000000000;
-    const chunk HALF_CHUNK = MAX_CHUNK / 2;
-    const chunk ALMOST_MAX_CHUNK = MAX_CHUNK - 1;
-
-    const size_t MAX_CHUNKS = PRECISION / BASE + 1;
-    const size_t MIN_CHUNKS = 1;
-    const int32_t MAX_SHIFT = static_cast<int32_t>( MAX_CHUNKS );
-
-    const size_t MAX_DIGITS = MAX_CHUNKS * BASE;
-    const int32_t MAX_EXP = MAX_DIGITS;
-
-    enum BigNumberType {
+    enum class BigNumberType : uint8_t {
         DEFAULT,
         ZERO,
         INF,
