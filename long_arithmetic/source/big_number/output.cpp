0003803fa21f85fb40e88c8cc690cecb59e62e34
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-22 01:53:01 +0300

    Refactor

--- long_arithmetic/source/big_number/output.cpp
+++ long_arithmetic/source/big_number/output.cpp
@@ -6,33 +6,31 @@
 #include "getters.hpp"
 
 namespace big_number {
-    int32_t compute_exp( const BigNumber& number ) {
+    int32_t compute_exponent( const BigNumber& number ) {
         return get_shift( number ) * BASE;
     }
 
-    std::string to_string_sign( bool is_negative ) {
+    std::string get_sign_string( bool is_negative ) {
         return is_negative ? MINUS_STR : EMPTY_STR;
     }
 
-    std::string to_string_exp( const BigNumber& number ) {
-        int32_t exp = compute_exp( number );
-        if ( exp == ZERO_INT ) return EMPTY_STR;
+    std::string get_exponent_string( const BigNumber& number ) {
+        int32_t exponent = compute_exponent( number );
+        if ( exponent == ZERO_INT ) return EMPTY_STR;
 
-        return EXP_STR + to_string_sign( exp < ZERO_INT ) +
-               std::to_string( exp );
+        return EXP_STR + get_sign_string( exponent < ZERO_INT ) +
+               std::to_string( exponent );
     }
 
-    std::string to_string_chunk( chunk value, bool is_first ) {
+    std::string format_chunk( chunk value, bool is_first ) {
         std::string str = std::to_string( value );
         if ( is_first ) return str;
 
-        while ( str.length() < BASE ) {
-            str = ZERO_CHAR + str;
-        }
+        str.insert( str.begin(), BASE - str.length(), ZERO_CHAR );
         return str;
     }
 
-    std::string to_string_mantissa( const BigNumber& number ) {
+    std::string get_mantissa_string( const BigNumber& number ) {
         size_t size = get_size( number );
         if ( size == ZERO_INT ) return EMPTY_STR;
 
@@ -40,38 +38,36 @@ namespace big_number {
         size_t first_chunk_idx = size - ONE_INT;
         std::string str;
 
+        str.reserve( size * BASE );
+
         for ( size_t index = size - ONE_INT; index > ZERO_INT; --index ) {
-            chunk value = mantissa[index];
-            str += to_string_chunk( value, first_chunk_idx == index );
+            str += format_chunk( mantissa[index], first_chunk_idx == index );
         }
-        str += to_string_chunk( mantissa[0], 0 == first_chunk_idx );
+        str += format_chunk( mantissa[0], 0 == first_chunk_idx );
 
         return str;
     }
 
-    std::string to_string_special( const BigNumber& number ) {
+    std::string get_special_string( const BigNumber& number ) {
         switch ( get_type( number ) ) {
         case BigNumberType::NOT_A_NUMBER:
             return NAN_STR;
-
         case BigNumberType::INF:
-            return to_string_sign( is_negative( number ) ) + INF_STR;
-
+            return get_sign_string( is_negative( number ) ) + INF_STR;
         case BigNumberType::ZERO:
             return ZERO_STR;
-
         default:
             return EMPTY_STR;
         }
     }
 
     std::string to_string( const BigNumber& number ) {
-        if ( is_special( number ) ) return to_string_special( number );
+        if ( is_special( number ) ) return get_special_string( number );
 
-        std::string sign = to_string_sign( is_negative( number ) );
-        std::string mantissa = to_string_mantissa( number );
-        std::string exp = to_string_exp( number );
+        std::string sign = get_sign_string( is_negative( number ) );
+        std::string mantissa = get_mantissa_string( number );
+        std::string exponent = get_exponent_string( number );
 
-        return sign + mantissa + exp;
+        return sign + mantissa + exponent;
     }
 }
