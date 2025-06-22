21cdb9ca1ba2077990d6adfdf4162f97f33888ee
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-22 23:56:42 +0300

    Format test `tools.hpp`

--- tests/tests/tools.hpp
+++ tests/tests/tools.hpp
@@ -3,6 +3,6 @@
 using namespace big_number;
 
 BigNumber create_big_number( const chunks& chunks,
-                           int32_t shift,
-                           bool is_negative = false,
-                           BigNumberType type = BigNumberType::DEFAULT );
+                             int32_t shift,
+                             bool is_negative = false,
+                             BigNumberType type = BigNumberType::DEFAULT );
