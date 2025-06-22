35f6dbb0c08f4d9831e3041fbab929b7e0fc5f62
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-22 22:12:53 +0300

    Refactor tests

--- tests/tests/abs.cpp
+++ tests/tests/abs.cpp
@@ -7,84 +7,73 @@
 
 using namespace big_number;
 
-class BigNumberAbsTest : public ::testing::Test {};
+class BigNumberAbsTest : public ::testing::Test {
+protected:
+    Error error = get_default_error();
+};
 
 TEST_F( BigNumberAbsTest, PositiveNumberRemainsUnchanged ) {
     chunks chunks = { 123 };
     auto number = create_big_number( chunks, 0, false );
+    auto expected = create_big_number( chunks, 0, false );
 
     auto result = abs( number );
 
-    EXPECT_EQ( result.mantissa, chunks );
-    EXPECT_EQ( result.shift, 0 );
-    EXPECT_FALSE( result.is_negative );
-    EXPECT_EQ( result.type, BigNumberType::DEFAULT );
+    EXPECT_TRUE( is_equal( result, expected ) );
 }
 
 TEST_F( BigNumberAbsTest, NegativeNumberBecomesPositive ) {
     chunks chunks = { 456 };
     auto number = create_big_number( chunks, 0, true );
+    auto expected = create_big_number( chunks, 0, false );
 
     auto result = abs( number );
 
-    EXPECT_EQ( result.mantissa, chunks );
-    EXPECT_EQ( result.shift, 0 );
-    EXPECT_FALSE( result.is_negative );
-    EXPECT_EQ( result.type, BigNumberType::DEFAULT );
-}
-
-TEST_F( BigNumberAbsTest, ZeroRemainsZero ) {
-    auto number = make_zero( Error{} );
-
-    auto result = abs( number );
-
-    EXPECT_EQ( result.mantissa, number.mantissa );
-    EXPECT_EQ( result.shift, number.shift );
-    EXPECT_FALSE( result.is_negative );
-    EXPECT_EQ( result.type, BigNumberType::ZERO );
-}
-
-TEST_F( BigNumberAbsTest, NaNRemainsNaN ) {
-    auto number = make_nan( Error{} );
-
-    auto result = abs( number );
-
-    EXPECT_EQ( result.mantissa, number.mantissa );
-    EXPECT_EQ( result.shift, number.shift );
-    EXPECT_FALSE( result.is_negative );
-    EXPECT_EQ( result.type, BigNumberType::NOT_A_NUMBER );
-}
-
-TEST_F( BigNumberAbsTest, PositiveInfinityRemainsPositiveInfinity ) {
-    auto number = make_inf( Error{}, false );
-
-    auto result = abs( number );
-
-    EXPECT_EQ( result.mantissa, number.mantissa );
-    EXPECT_EQ( result.shift, number.shift );
-    EXPECT_FALSE( result.is_negative );
-    EXPECT_EQ( result.type, BigNumberType::INF );
-}
-
-TEST_F( BigNumberAbsTest, NegativeInfinityBecomesPositiveInfinity ) {
-    auto number = make_inf( Error{}, true );
-
-    auto result = abs( number );
-
-    EXPECT_EQ( result.mantissa, number.mantissa );
-    EXPECT_EQ( result.shift, number.shift );
-    EXPECT_FALSE( result.is_negative );
-    EXPECT_EQ( result.type, BigNumberType::INF );
+    EXPECT_TRUE( is_equal( result, expected ) );
 }
 
 TEST_F( BigNumberAbsTest, NumberWithShiftRemainsUnchanged ) {
     chunks chunks = { 789 };
     auto number = create_big_number( chunks, 2, true );
+    auto expected = create_big_number( chunks, 2, false );
 
     auto result = abs( number );
 
-    EXPECT_EQ( result.mantissa, chunks );
-    EXPECT_EQ( result.shift, 2 );
-    EXPECT_FALSE( result.is_negative );
-    EXPECT_EQ( result.type, BigNumberType::DEFAULT );
+    EXPECT_TRUE( is_equal( result, expected ) );
+}
+
+TEST_F( BigNumberAbsTest, ZeroRemainsZero ) {
+    auto number = make_zero( error );
+    auto expected = make_zero( error );
+
+    auto result = abs( number );
+
+    EXPECT_TRUE( is_equal( result, expected ) );
+}
+
+TEST_F( BigNumberAbsTest, NaNRemainsNaN ) {
+    auto number = make_nan( error );
+    auto expected = make_nan( error );
+
+    auto result = abs( number );
+
+    EXPECT_TRUE( is_equal( result, expected ) );
+}
+
+TEST_F( BigNumberAbsTest, PositiveInfinityRemainsPositiveInfinity ) {
+    auto number = make_inf( error, false );
+    auto expected = make_inf( error, false );
+
+    auto result = abs( number );
+
+    EXPECT_TRUE( is_equal( result, expected ) );
+}
+
+TEST_F( BigNumberAbsTest, NegativeInfinityBecomesPositiveInfinity ) {
+    auto number = make_inf( error, true );
+    auto expected = make_inf( error, false );
+
+    auto result = abs( number );
+
+    EXPECT_TRUE( is_equal( result, expected ) );
 }
