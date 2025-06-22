9802d580e79b0c7e95c9fa3c7362f38e7db488f8
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-22 20:11:48 +0300

    Update `is_lower_than`, fix issues, refactor

--- long_arithmetic/source/big_number/is_lower_than.cpp
+++ long_arithmetic/source/big_number/is_lower_than.cpp
@@ -4,35 +4,13 @@
 #include "getters.hpp"
 
 namespace big_number {
-    bool has_lower_than_mantissa( const BigNumber& lhs, const BigNumber& rhs ) {
-        const chunks& lhs_mantissa = get_mantissa( lhs );
-        const chunks& rhs_mantissa = get_mantissa( rhs );
-        size_t lhs_size = lhs_mantissa.size();
-        size_t rhs_size = rhs_mantissa.size();
-        size_t min_size = std::min( lhs_size, rhs_size );
-
-        if ( lhs_size != rhs_size ) return min_size == lhs_size;
-
-        for ( size_t delta = 0; delta <= min_size; delta++ ) {
-            if ( lhs_mantissa[lhs_size - delta] <
-                 rhs_mantissa[rhs_size - delta] )
-                return true;
-        }
-
-        return false;
+    bool has_lower_power( const BigNumber& lhs, const BigNumber& rhs ) {
+        int32_t lhs_power = count_power( lhs );
+        int32_t rhs_power = count_power( rhs );
+        return ( lhs_power != rhs_power ) ? lhs_power < rhs_power : false;
     }
 
-    bool has_lower_than_size( const BigNumber& lhs, const BigNumber& rhs ) {
-        int32_t lhs_size = get_size( lhs ) + get_shift( lhs );
-        int32_t rhs_size = get_size( rhs ) + get_shift( rhs );
-        return lhs_size < rhs_size;
-    }
-
-    bool has_lower_than_sign( const BigNumber& lhs, const BigNumber& rhs ) {
-        return is_negative( lhs ) < is_negative( rhs );
-    }
-
-    bool is_lower_than_special( const BigNumber& lhs, const BigNumber& rhs ) {
+    bool is_lower_special( const BigNumber& lhs, const BigNumber& rhs ) {
         if ( is_nan( lhs ) || is_nan( rhs ) ) return false;
 
         switch ( get_type( lhs ) ) {
@@ -41,7 +19,7 @@ namespace big_number {
                                  : is_negative( lhs );
 
         case BigNumberType::ZERO:
-            return is_zero( rhs ) ? false : is_negative( rhs );
+            return is_zero( rhs ) ? false : !is_negative( rhs );
 
         default:
             if ( is_inf( rhs ) ) return !is_negative( rhs );
@@ -52,12 +30,30 @@ namespace big_number {
 
     bool is_lower_than( const BigNumber& lhs, const BigNumber& rhs ) {
         if ( is_special( lhs ) || is_special( rhs ) )
-            return is_lower_than_special( lhs, rhs );
+            return is_lower_special( lhs, rhs );
 
-        if ( has_lower_than_sign( lhs, rhs ) ) return true;
-        if ( has_lower_than_size( lhs, rhs ) ) return true;
-        if ( has_lower_than_mantissa( lhs, rhs ) ) return true;
+        if ( is_negative( lhs ) != is_negative( rhs ) )
+            return is_negative( lhs ) > is_negative( rhs );
 
+        if ( has_lower_power( lhs, rhs ) )
+            return is_negative( lhs ) ? false : true;
+
+        const chunks& lhs_mantissa = get_mantissa( lhs );
+        const chunks& rhs_mantissa = get_mantissa( rhs );
+        size_t lhs_size = lhs_mantissa.size();
+        size_t rhs_size = rhs_mantissa.size();
+        size_t min_size = std::min( lhs_size, rhs_size );
+
+        if ( lhs_size != rhs_size ) return min_size == lhs_size;
+
+        for ( size_t delta = 1; delta <= min_size; delta++ ) {
+            chunk lhs_chunk = lhs_mantissa[lhs_size - delta];
+            chunk rhs_chunk = rhs_mantissa[rhs_size - delta];
+            if ( lhs_chunk != rhs_chunk ) {
+                bool is_chunk_lower = lhs_chunk < rhs_chunk;
+                return is_negative( lhs ) ? !is_chunk_lower : is_chunk_lower;
+            }
+        }
         return false;
     }
 }
