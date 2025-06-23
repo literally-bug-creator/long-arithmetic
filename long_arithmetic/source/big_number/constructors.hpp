421e5a9a7355ca4a683edeb914f4e554fbf66ab4
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-23 23:08:10 +0300

    Remove special constructors from `constructors.hpp`

--- long_arithmetic/source/big_number/constructors.hpp
+++ long_arithmetic/source/big_number/constructors.hpp
@@ -3,12 +3,6 @@
 #include "big_number.hpp"
 
 namespace big_number {
-    BigNumber make_zero( const Error& error );
-
-    BigNumber make_inf( const Error& error, bool is_negative );
-
-    BigNumber make_nan( const Error& error );
-
     BigNumber make_big_number( chunks chunks,
                                int32_t shift,
                                BigNumberType type,
